820554f8b465dc59cc9af0dbfc616b498b3f9b36
fantimond <fantimond@gmail.com>
2020-06-03 22:02:06 +0800

    modify singletri.cpp

--- src/singletri/singletri.cpp
+++ src/singletri/singletri.cpp
@@ -23,81 +23,72 @@
 
 #include <sb7.h>
 
-class SingleTriangleApp : public sb7::application
-{
-    void init()
-    {
-        static const char title[] = "OpenGL SuperBible - Single Triangle";
+class SingleTriangleApp : public sb7::application {
+  void init() {
+    static const char title[] = "OpenGL SuperBible - Single Triangle";
 
-        sb7::application::init();
+    sb7::application::init();
 
-        memcpy(info.title, title, sizeof(title));
-    }
+    memcpy(info.title, title, sizeof(title));
+  }
 
-    virtual void startup()
-    {
-        static const char * vs_source[] =
-        {
-            "#version 420 core                                                 \n"
-            "                                                                  \n"
-            "void main(void)                                                   \n"
-            "{                                                                 \n"
-            "    const vec4 vertices[] = vec4[](vec4( 0.25, -0.25, 0.5, 1.0),  \n"
-            "                                   vec4(-0.25, -0.25, 0.5, 1.0),  \n"
-            "                                   vec4( 0.25,  0.25, 0.5, 1.0)); \n"
-            "                                                                  \n"
-            "    gl_Position = vertices[gl_VertexID];                          \n"
-            "}                                                                 \n"
-        };
+  virtual void startup() {
+    static const char *vs_source[] = {
+        "#version 410 core                                                 \n"
+        "                                                                  \n"
+        "void main(void)                                                   \n"
+        "{                                                                 \n"
+        "    const vec4 vertices[] = vec4[](vec4( 0.25, -0.25, 0.5, 1.0),  \n"
+        "                                   vec4(-0.25, -0.25, 0.5, 1.0),  \n"
+        "                                   vec4( 0.25,  0.25, 0.5, 1.0)); \n"
+        "                                                                  \n"
+        "    gl_Position = vertices[gl_VertexID];                          \n"
+        "}                                                                 \n"};
 
-        static const char * fs_source[] =
-        {
-            "#version 420 core                                                 \n"
-            "                                                                  \n"
-            "out vec4 color;                                                   \n"
-            "                                                                  \n"
-            "void main(void)                                                   \n"
-            "{                                                                 \n"
-            "    color = vec4(0.0, 0.8, 1.0, 1.0);                             \n"
-            "}                                                                 \n"
-        };
+    static const char *fs_source[] = {
+        "#version 410 core                                                 \n"
+        "                                                                  \n"
+        "out vec4 color;                                                   \n"
+        "                                                                  \n"
+        "void main(void)                                                   \n"
+        "{                                                                 \n"
+        "    color = vec4(0.0, 0.8, 1.0, 1.0);                             \n"
+        "}                                                                 \n"};
 
-        program = glCreateProgram();
-        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fs, 1, fs_source, NULL);
-        glCompileShader(fs);
+    program = glCreateProgram();
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, fs_source, NULL);
+    glCompileShader(fs);
 
-        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vs, 1, vs_source, NULL);
-        glCompileShader(vs);
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, vs_source, NULL);
+    glCompileShader(vs);
 
-        glAttachShader(program, vs);
-        glAttachShader(program, fs);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
 
-        glLinkProgram(program);
+    glLinkProgram(program);
 
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-    }
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+  }
 
-    virtual void render(double currentTime)
-    {
-        static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
-        glClearBufferfv(GL_COLOR, 0, green);
+  virtual void render(double currentTime) {
+    static const GLfloat green[] = {0.0f, 0.25f, 0.0f, 1.0f};
+    glClearBufferfv(GL_COLOR, 0, green);
 
-        glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-    }
+    glUseProgram(program);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+  }
 
-    virtual void shutdown()
-    {
-        glDeleteVertexArrays(1, &vao);
-        glDeleteProgram(program);
-    }
+  virtual void shutdown() {
+    glDeleteVertexArrays(1, &vao);
+    glDeleteProgram(program);
+  }
 
 private:
-    GLuint          program;
-    GLuint          vao;
+  GLuint program;
+  GLuint vao;
 };
 
 DECLARE_MAIN(SingleTriangleApp)
